f51310a231b7b41070ccec8eac27910aed7751fb
saketh <sakethreddymacha36@gmail.com>
2025-11-01 17:44:19 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using PresentationLayer;
+
+namespace ApplicationUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInputManager inputs = new UserInputManager();
+            inputs.InputReader();
+        }
+    }
+}
